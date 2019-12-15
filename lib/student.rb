db2ab93529951c0fb94f560dd1a1fa56e6c3e985
Randy Steele <steelerx155@gmail.com>
2019-12-15 14:54:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,8 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
-  def self.create(id)
+  def self.create(name, grade)
+    {name =>
     student = Student.new(id)
     student.save
     student
@@ -44,5 +45,20 @@ end
 
 
 
-
+describe ".create" do
+    before(:each) do
+      Student.create_table
+    end
+    it 'takes in a hash of attributes and uses metaprogramming to create a new student object. Then it uses the #save method to save that student to the database' do
+      Student.create(name: "Sally", grade: "10th")
+      expect(DB[:conn].execute("SELECT * FROM students")).to eq([[1, "Sally", "10th"]])
+    end
+    it 'returns the new object that it instantiated' do
+      student = Student.create(name: "Josh", grade: "9th")
+      expect(student).to be_a(Student)
+      expect(student.name).to eq("Josh")
+      expect(student.grade).to eq("9th")
+    end
+  end
+end
 
